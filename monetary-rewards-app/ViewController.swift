8e8aa64219f841811a9feb74265394be4cf314ad
Bruce Frick <bfrick22@gmail.com>
2017-01-19 23:26:26 -0800

    Adding comments and properties

--- monetary-rewards-app/ViewController.swift
+++ monetary-rewards-app/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var rewardNameLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +23,11 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //MARK: Actions
+    @IBAction func setDefaultLabelText(sender: AnyObject) {
+    }
+    
 
 
 }
